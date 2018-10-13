7cb9fb1ee4042f6d07ced46e414df293e6bcd50e
SitiSchu <admin@sitischu.com>
2018-10-14 01:03:09 +0200

    Bump version in jdb itself too
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jdb",
-    version="0.2.0",
+    version="0.2.1",
     author="Simon Schürrle",
     author_email="info@sitischu.com",
     description="Simple json based database",
